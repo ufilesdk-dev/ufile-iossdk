a9f513a4263877073a539020290d6c6b9b97059b
aplaycat <tuandxiong@gmail.com>
2018-01-02 16:16:48 +0800

    1.增加接口参数说明

--- ucloud-ufile-sdk/OC/UFileSDK/UFileSDK.h
+++ ucloud-ufile-sdk/OC/UFileSDK/UFileSDK.h
@@ -16,8 +16,8 @@
 /**
  UFileSDK 初始化
 
- @param strSignServerUrl 签名服务器地址
- @param bucket 配置的bucket
+ @param strSignServerUrl 签名服务器地址 （必须）
+ @param bucket 配置的bucket （必须）
  @return 返回UFileSDK实例
  */
 -(instancetype)initWith:(NSString *)strSignServerUrl Bucket:(NSString *)bucket;
@@ -26,16 +26,17 @@
 
 /**
  计算签名
- @param httpMethod  http请求方法
- @param key 文件名（包含后缀）
- @param contentMd5 MD5内容可以为nil
- @param contentType 内容类型
- @param policy 回掉策略 参数类型是 NSDictionary,例如
+ @param httpMethod  http请求方法（必须）
+ @param key 文件名（包含后缀 必须）
+ @param contentMd5 MD5内容可以为nil（可选）
+ @param contentType 内容类型 （可选）
+ @param policy 回掉策略 （可选）
+       设置参数类型是 NSDictionary,例如
        {
-         "callbackUrl" : "http://test.ucloud.cn",   //指定回调服务的地址
-         "callbackBody" : "key1=value1&key2=value2" //传递给回调服务的参数
+         "callbackUrl" : "http://test.ucloud.cn",   //指定回调服务的地址 （必须）
+         "callbackBody" : "key1=value1&key2=value2" //传递给回调服务的参数 （必须）
        }
-       注意的是需要回掉策略的有putFile和multipartUploadFinish两个API,其他API,这个参数可以设置nil
+       注意的是支持回掉策略的有putFile和multipartUploadFinish两个API,此参数必须,其他API,这个参数设置nil
  @return 返回签名字符串
  */
 -(NSString*)calcKey:(NSString*)httpMethod  Key:(NSString*)key  MD5:(NSString*)contentMd5 ContentType:(NSString*)contentType CallBackPolicy:(NSDictionary*)policy;
